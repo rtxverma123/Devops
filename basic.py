fa377cb8e515e541d28262275cb84fd6081720b6
rtxverma123 <tamishverma@outlook.com>
2021-02-02 01:07:49 +0530

    added new code

--- basic.py
+++ basic.py
@@ -10,5 +10,19 @@ for i in tup1:
      print(tup1)
 
 
+def getProduct(n): 
+  
+    product = 1
+  
+    while (n != 0): 
+        product = product * (n % 10) 
+        n = n // 10
+  
+    return product 
+  
+# Driver Code 
+n = 4513
+print(getProduct(n)) 
+
 
 
