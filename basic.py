5accdc3d491b3a7069970355639846a274174be3
rtxverma123 <tamishverma@outlook.com>
2021-02-02 00:50:45 +0530

    added tuple

---
+++ basic.py
@@ -0,0 +1,14 @@
+lst = [1,2,3,4,5]
+def mul(x):
+     return x*4
+result = map(mul,lst)
+res = list(result)
+a = str(res)[1:-1]
+print(a)
+tup1 = ('apple','banna', 'orange')
+for i in tup1:
+     print(tup1)
+
+
+
+
